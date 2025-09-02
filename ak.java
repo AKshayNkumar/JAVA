be2d1dfe7b41afae1b21321fecb1a5a9bc6d1a19
AKshayNkumar <akshayncc12345@gmail.com>
2025-09-02 15:33:44 +0530

    Add files via upload

---
+++ ak.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class ak {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the name : ");
+        String name = sc.next();
+        System.out.println("Length : "+name.length());
+
+    }
+    
+}
